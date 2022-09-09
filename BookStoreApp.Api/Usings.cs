caedee33895312d1119f8269d73c306c0d5381ea
Thome Valentin <thomeval@hotmail.com>
2022-09-09 11:07:42 +0200

    Add project files.

---
+++ BookStoreApp.Api/Usings.cs
@@ -0,0 +1,5 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.Extensions.Logging;
---
+++ BookStoreApp.Api/WeatherForecast.cs
@@ -0,0 +1,15 @@
+namespace BookStoreApp.Api;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF
+    {
+        get { return 32 + (int)(TemperatureC / 0.5556); }
+    }
+
+    public string? Summary { get; set; }
+}
